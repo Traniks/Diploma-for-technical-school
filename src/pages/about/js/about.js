32071a2d3670ea56e654c604f990a3afe9da41cc
Traniks <andert2017@gmail.com>
2024-05-26 20:12:49 +0300

    feat: add about + programs

--- src/pages/about/js/about.js
+++ src/pages/about/js/about.js
@@ -1,3 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
+import Swiper from 'swiper'
+import {Pagination,Navigation,Autoplay,EffectCards,} from 'swiper/modules'
 
+document.addEventListener('DOMContentLoaded', () => {
+	const swiperAbout = new Swiper('#about-slider', {
+		modules: [Pagination, Navigation, EffectCards],
+		speed: 500,
+		effect: 'cards',
+		grabCursor: true,
+		rewind: true,
+
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		cardsEffect: {
+			perSlideOffset: 10,
+			perSlideRotate: 2,
+			slideShadows: false,
+			rotate: true,
+		},
+	})
+
+	const swiperAbout2 = new Swiper('#about-slider2', {
+		modules: [Pagination, Navigation, Autoplay],
+		speed: 500,
+		grabCursor: true,
+		rewind: true,
+		slidesPerView: 3,
+		spaceBetween: 30,
+
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	})
 })
--- src/pages/index/js/index.js
+++ src/pages/index/js/index.js
@@ -1,3 +1,57 @@
+import Swiper from 'swiper'
+import {Pagination,Navigation,Autoplay,EffectCoverflow,} from 'swiper/modules'
+
 document.addEventListener('DOMContentLoaded', () => {
-	
+	const swiperHero1 = new Swiper('#hero-slider', {
+		modules: [Pagination, Navigation, EffectCoverflow, Autoplay],
+		speed: 500,
+		effect: 'coverflow',
+		slidesPerView: '1',
+		grabCursor: true,
+		centeredSlides: true,
+		rewind: true,
+
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+
+		coverflowEffect: {
+			rotate: 0,
+			stretch: 0,
+			depth: 100,
+			scale: 0.8,
+			modifier: 1,
+			slideShadows: false,
+		},
+		breakpoints: {
+			1200: {
+				slidesPerView: '3',
+			},
+		},
+	})
+
+	// Функция для установки значения num в зависимости от размера экрана
+	function setNum() {
+		if (window.innerWidth > 1200) {
+			swiperHero1.slideTo(1)
+		} else {
+			swiperHero1.slideTo(0)
+		}
+	}
+
+	// Вызываем функцию при загрузке страницы
+	window.addEventListener('load', setNum)
+	// Вызываем функцию при изменении размера окна
+	window.addEventListener('resize', setNum)
 })
